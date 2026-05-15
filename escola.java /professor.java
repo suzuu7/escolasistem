1c2e09e05817c3e3638f0e764696600885d301f6
suzuu7 <psuzane91@gmail.com>
2026-05-14 23:42:29 -0300

    professor class

---
+++ escola.java /professor.java
@@ -0,0 +1,30 @@
+public class Professor extends Aluno {
+
+    private String titulo;
+
+    public Professor(String nome, String fone,
+                     String cpf, String mtr,
+                     String titulo) {
+
+        super(nome, fone, cpf, mtr);
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public String saudacao() {
+
+        return "Olá, sou o professor "
+                + getNome()
+                + ", título: "
+                + titulo;
+    }
+
+    public String ensina() {
+
+        return getNome() + " está ensinando.";
+    }
+}
