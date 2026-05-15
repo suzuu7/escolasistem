2af6247e52c186c945d889f817b97309ebe681d1
suzuu7 <psuzane91@gmail.com>
2026-05-14 23:39:56 -0300

    responsavel class

---
+++ escola.java /responsavel.java
@@ -0,0 +1,38 @@
+public class Responsavel extends Pessoa {
+
+    private Aluno[] alunos;
+
+    public Responsavel(String nome, String fone,
+                       String cpf, Aluno[] alunos) {
+
+        super(nome, fone, cpf);
+        this.alunos = alunos;
+    }
+
+    public Aluno[] getAlunos() {
+        return alunos;
+    }
+
+    @Override
+    public String saudacao() {
+
+        return "Olá, sou responsável pelos alunos.";
+    }
+
+    public String mostrarAlunos() {
+
+        String texto =
+                "Alunos do responsável "
+                + getNome()
+                + ":\n";
+
+        for (Aluno aluno : alunos) {
+
+            texto += "- "
+                    + aluno.getNome()
+                    + "\n";
+        }
+
+        return texto;
+    }
+}
