188ab754950505ca5cd688584d318092de703d08
suzuu7 <psuzane91@gmail.com>
2026-05-14 23:41:16 -0300

    funcionario class

---
+++ escola.java /funcionario.java
@@ -0,0 +1,37 @@
+public class Funcionario extends Pessoa {
+
+    private String cod;
+    private String funcao;
+
+    public Funcionario(String nome, String fone,
+                       String cpf, String cod,
+                       String funcao) {
+
+        super(nome, fone, cpf);
+
+        this.cod = cod;
+        this.funcao = funcao;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    @Override
+    public String saudacao() {
+
+        return "Olá, sou funcionário e trabalho como "
+                + funcao;
+    }
+
+    public String desempenhaFuncao() {
+
+        return getNome()
+                + " trabalha como "
+                + funcao;
+    }
+}
